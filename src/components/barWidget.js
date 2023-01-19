923a74d39be15d1aa8c6cce20f0d4f107c4f1ff1
anuragmathews08 <anuragmathews619@gmail.com>
2023-01-20 01:03:54 +0530

    bar range selector developed

--- src/components/barWidget.js
+++ src/components/barWidget.js
@@ -1,3 +1,22 @@
+import { WidgetBox } from "../hoc";
+import { BarUI } from "./barUI";
+import { useState } from "react";
+
 export const BarWidget = () => {
-  return <h1>Bar Widget</h1>;
+  const [rangeValue, setRangeValue] = useState(3);
+
+  return (
+    <WidgetBox>
+      <h3 className="text-white text-xl font-medium">
+        {rangeValue >= 1 && rangeValue < 3 ? (
+          <>Low</>
+        ) : rangeValue >= 3 && rangeValue < 5 ? (
+          <>Medium</>
+        ) : (
+          <>High</>
+        )}
+      </h3>
+      <BarUI selected={rangeValue} handleValue={setRangeValue} />
+    </WidgetBox>
+  );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export * from "./circleWidget";
 export * from "./barWidget";
 export * from "./circleUI";
 export * from "./rangeSlider";
+export * from "./barUI";
